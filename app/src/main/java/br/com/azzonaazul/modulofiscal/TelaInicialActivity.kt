02971f3bffd05fcd74c61fad008b14c60c691b9a
AleqsiTowers <alextorres7736@gmail.com>
2022-03-17 20:43:09 -0300

    tela inicial

---
+++ app/src/main/java/br/com/azzonaazul/modulofiscal/TelaInicialActivity.kt
@@ -0,0 +1,11 @@
+package br.com.azzonaazul.modulofiscal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaInicialActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_inicial)
+    }
+}
