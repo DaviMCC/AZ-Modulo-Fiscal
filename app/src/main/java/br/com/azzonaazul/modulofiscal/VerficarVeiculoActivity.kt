ccb78807637cda55cba4133660eeafb9cd10285a
Davi Carboni <davicarlosm.carboni@gmail.com>
2022-03-17 17:24:01 -0300

    Inicio verificar veiculo

---
+++ app/src/main/java/br/com/azzonaazul/modulofiscal/VerficarVeiculoActivity.kt
@@ -0,0 +1,11 @@
+package br.com.azzonaazul.modulofiscal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VerficarVeiculoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verficar_veiculo)
+    }
+}
